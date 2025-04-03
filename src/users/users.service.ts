2e0e2893ae41dc770bc9fe4ba08a76492ddf0793
Lucas Shahbazi <lucasshahbazi@yahoo.de>
2025-04-03 21:22:11 +0200

    changed function names to loginUser and createUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,7 +9,7 @@ import { JwtService } from '@nestjs/jwt';
 export class UsersService {
   constructor(private prisma: PrismaService, private jwtService: JwtService) {} 
 
-  async CreateUser(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto) {
     const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
     try {
         const user = await this.prisma.user.create({
@@ -26,5 +26,20 @@ export class UsersService {
         throw error;
     }
   }
-
+  async loginUser(loginUserDto: LoginUserDto) {
+    const user = await this.prisma.user.findUnique({
+        where: {email: loginUserDto.email},
+    });
+    if (!user) {
+        throw new UnauthorizedException('User does not exist');
+    }
+    const isPasswordValid = await bcrypt.compare(loginUserDto.password, user.password); 
+    if (!isPasswordValid) {
+        throw new UnauthorizedException('Invalid password');
+    }
+    const payload = {sub: user.id, email: user.email};
+    return {
+        access_token: await this.jwtService.signAsync(payload);
+    }
+  }
 }
