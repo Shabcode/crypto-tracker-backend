cb85ebe463f55ffdb9264d4834ea6b515934ad46
Lucas Shahbazi <lucasshahbazi@yahoo.de>
2025-04-03 22:01:38 +0200

    created controller functions to handle registration and login requests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Post('register')
+    async register(@Body() createUserDto: CreateUserDto) {
+      return this.usersService.createUser(createUserDto);
+    }
+  
+    @Post('login')
+    async login(@Body() loginUserDto: LoginUserDto) {
+      return this.usersService.loginUser(loginUserDto);
+    }
+  }
