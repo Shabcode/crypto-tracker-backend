02cf5da3d63b3495b5fbbc5888822577e0719d8b
Lucas Shahbazi <lucasshahbazi@yahoo.de>
2025-04-03 21:02:55 +0200

    updated imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [
+    UsersModule,
+    PrismaModule,
+    JwtModule.register({
+      global: true,
+      secret: 'yourSecretKey',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
